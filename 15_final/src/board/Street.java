f25199df3c0d0b3a3547f5f1d2e4a6ef9e93f1ad
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2017-01-04 16:07:08 +0100

    made buyHouses method

--- 15_final/src/board/Street.java
+++ 15_final/src/board/Street.java
@@ -1,13 +1,16 @@
 package board;
+
+import java.util.ArrayList;
+
 /**
  * Class Street, which has a type, and can have houses and a hotel built on it.
  * @author Mathias Tvaermose Gleerup
  *
  */
 public class Street extends Ownable {
+	int priceOfBuilding;
 	int [] rents;
-	char type;
-	int i=0;
+	int numberOfBuildings=0;
 	
 	/**
 	 * Constructor which instantiates a Street.
@@ -20,9 +23,8 @@ public class Street extends Ownable {
 	 * @param type
 	 */
 	public Street(String name, int id,int price, int pawn, int[] rents, char type) {
-		super(name,id, price, pawn);
+		super(name,id, price, pawn,type);
 		this.rents=rents;
-		this.type=type;
 	}
 
 	@Override
@@ -31,7 +33,30 @@ public class Street extends Ownable {
 		return 0;
 	}
 	
-	public int buyHouses(){
-		
+	public int buyHouses(int amount){
+		boolean permission =false;
+		int maxAmount;
+		ArrayList<Ownable> arr = owner.getOwned();
+		int counter=0;
+		for(int i =0; i<=arr.size();i++){
+			if(arr.get(i).getType()==this.type){
+				counter++;
+			}
+		}
+		if(this.type=='A'||this.type=='H'){
+			maxAmount=2;
+		}else{
+			maxAmount =3;
+		}
+		if(counter==maxAmount){
+			permission=true;
+		}
+		if(permission=true){
+			numberOfBuildings+=amount;
+			owner.pay(amount*priceOfBuilding);
+			return amount*priceOfBuilding;
+		}else{
+			return 0;
+		}
 	}
 }
