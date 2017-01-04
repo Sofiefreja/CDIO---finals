6af85d89d727c2c5cfb1651904f6c487758bc585
emilyrasmussen <emilyrasmussen@emilys-air.clients.wireless.dtu.dk>
2017-01-04 15:08:33 +0100

    Creating of the board

---
+++ 15_final/src/controller/testBoard.java
@@ -0,0 +1,16 @@
+package controller;
+
+import desktop_resources.GUI;
+
+public class testBoard {
+
+	public static void main(String [ ] args)
+	{
+	     GUIControl testboard = new GUIControl();
+	    
+	     testboard.makeBoard();
+	     GUI.showMessage("Jeg er sej");
+	     
+	     
+	}
+}
