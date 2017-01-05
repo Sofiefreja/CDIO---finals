5022dee23e2eb0d8d2afb928d6d1b8c2f31ba629
Simon <Simon@10.16.73.166>
2017-01-05 13:13:41 +0100

    Finished first iteration of MobilePay

--- 15_final/src/cards/MobilePay.java
+++ 15_final/src/cards/MobilePay.java
@@ -1,7 +1,9 @@
 package cards;
 
+import java.util.ArrayList;
 import java.util.Random;
 
+import board.Ownable;
 import entities.Player;
 
 /**
@@ -12,9 +14,11 @@ import entities.Player;
 public class MobilePay extends Transaction{
 
 	int number = 0;
+	ArrayList<Player> playerList = new ArrayList<Player>();
 	
-	public MobilePay(String name, int money) {
+	public MobilePay(String name, int money, ArrayList<Player> playerList) {
 		super(name, money);
+		this.playerList = playerList;
 		
 	}
 
@@ -23,14 +27,42 @@ public class MobilePay extends Transaction{
 		
 		Random number = new Random();
 		int res = number.nextInt(3);
+		int amount = playerList.size();
 		
 		switch(res) {
 		
 		case 1:
-			GUIControl.pay200Message();
+			GUIControl.payBirthdayMessage();
 			
-			player.deposit(200*GameLogic.numberOfPlayers());
+			for (int i=0; i<=playerList.size();i++) {
+				playerList.get(i).withdraw(200);
+			}
+				
+			player.deposit(200*amount);
 			
+			break;
+			
+		case 2:
+			GUIControl.payFamilyMessage();
+			
+			for (int i=0; i<=playerList.size();i++) {
+				playerList.get(i).withdraw(500);
+			}
+				
+			player.deposit(500*amount);
+			
+			break;
+		
+		case 3:
+			GUIControl.payPartyMessage();
+			
+			for (int i=0; i<=playerList.size();i++) {
+				playerList.get(i).withdraw(500);
+			}
+				
+			player.deposit(500*amount);
+			
+			break;
 		
 		}
 			
