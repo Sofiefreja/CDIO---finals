b34ed06dbe18ee9ce08ddb1831fe625854acf57f
Sofie <Sofie@Sofies_Pc.eduroam.wireless.dtu.dk>
2017-01-05 11:22:18 +0100

    movetoShip

--- 15_final/src/cards/MovetoShip.java
+++ 15_final/src/cards/MovetoShip.java
@@ -7,21 +7,5 @@ import controller.GUIControl;
  *
  */
 public class MovetoShip extends Card{
-	private int shipNr;
 	
-	public MovetoShip(String name, int shipNr){
-		this.name = name;
-		this.shipNr = shipNr;
-	}
-	public void useCard(Player player){
-		if(Player.getCurrentPosition(player)<5 && Player.getCurrentPosition(player)>35){
-			Player.setPosition();
-		}else if(Player.getCurrentPosition(player)>5 && Player.getCurrentPosition(player)<15){
-			
-		}else if(Player.getCurrentPosition(player)>15 && Player.getCurrentPosition(player)<25){
-			
-		}else if(Player.getCurrentPosition(player)>25 && Player.getCurrentPosition(player)<35){
-			
-		}
-	}
 }
