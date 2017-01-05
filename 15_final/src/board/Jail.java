fe2ce8bd01aa820886326b1e1d94d78f5e4fa2e2
Simon <Simon@10.16.73.166>
2017-01-05 11:21:59 +0100

    Jail and Start

---
+++ 15_final/src/board/Jail.java
@@ -0,0 +1,28 @@
+package board;
+
+import entities.Player;
+import controller.GUIControl;
+
+/**
+ * 
+ * @author Simon Lundorf s154008 & Emily Skovgaard Rasmussen s153374
+ *
+ */
+
+public class Jail extends Square {
+
+	public Jail(String name, int id) {
+		super(name, id);
+	
+	}
+
+	@Override
+	public void landOnSquare(Player player) {
+		player.setJailStatus(true);
+		player.setPosition(10);
+	}
+
+		
+	}
+	
+
