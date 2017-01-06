25907f269229aa9a5d5eb1a6414af10476a5307b
Simon <Simon@10.16.73.166>
2017-01-06 15:46:27 +0100

    Small changes

--- 15_final/src/test/Buildable.java
+++ 15_final/src/test/Buildable.java
@@ -4,18 +4,30 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import board.Brewery;
+import board.Jail;
+import board.Ownable;
+import board.Parking;
+import board.Shipping;
+import board.Square;
+import board.Start;
+import board.Street;
+import board.Tax;
+import entities.Board;
 import entities.Player;
 
 public class Buildable {
-
+	
+	private Board theBoard;
+	
+	Player p1 = new Player("Simon");
+	
 	@Test
 	public void test() {
 		
-		Player p1 = new Player("Simon");
-		
-		p1.buySquare(square);
+		p1.buySquare(street[0]);
 		
 		
 	}
-
 }
+
