e0b2964563e68d12f07835f6f51bb5f839f9509c
emilyrasmussen <emilyrasmussen@emilys-air.clients.wireless.dtu.dk>
2017-01-06 10:00:29 +0100

    Update

--- 15_final/src/board/Brewery.java
+++ 15_final/src/board/Brewery.java
@@ -2,38 +2,5 @@ package board;
 
 public class Brewery extends Ownable {
 
-	private final int rent_1 = 1;
-	private final int rent_2 = 1;
-
-	public Brewery(String name, int id, int price, int pawn, char type) {
-		super(name, id, price, pawn, type);
-
-	}
-
-	@Override
-	public int getRent() {
-		int rent; 
-
-		if(get()){
-			rent = 0;
-
-		}
-		else { 
-			switch (()) {
-			case 1: 
-				rent = 100 * player.getDiceSum();
-				break;
-			case 2:
-				rent = 200 * player.getDiceSum();
-				break;
-			default: return 0;
-
-
-			}
-		}
-		
-		return rent;
-
-	}
 
 }
