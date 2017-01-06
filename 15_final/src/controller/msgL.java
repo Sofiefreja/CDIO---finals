f5bd7ccd45b4140a513450e602681c23a14f6635
Jonas Larsen <jonaslarsen@10.16.138.92>
2017-01-06 14:22:08 +0100

    Read from a CSV file

---
+++ 15_final/src/controller/msgL.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class msgL {
+	private static String[] info_danish = new String[1000];
+	private static String[] info_english = new String[1000];
+	
+	static {
+		System.out.println(new File("ajknsfhijoa").getAbsolutePath()); // FInd bib.
+		String fileName = "Lastfile.csv";
+		File file = new File(fileName);
+		int Danish = 2;
+		int English = 3; 
+		int i = 0;
+		try {
+			Scanner inputStream = new Scanner(file);
+			while (inputStream.hasNextLine()) {
+				info_danish[i++] = inputStream.nextLine().split(";")[Danish];
+				info_english[i++] = inputStream.nextLine().split(";")[English];
+			}
+			inputStream.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Forkert");
+		}
+		
+	}
+	public static String msg(String lang, int index){
+		if(lang.equals("en")){
+			return info_danish[index];
+		}
+		else{
+			return info_english[index];
+		}
+		
+	}
+	
+	
+
+	
+}
