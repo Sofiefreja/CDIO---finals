8ae26ed0cebdf29909bd6bc2bd7744d3013a54cb
Simon <Simon@10.16.73.166>
2017-01-06 14:46:14 +0100

    Created a test package, begun testing the buildable method and added method calls in Player

--- 15_final/src/entities/Assets.java
+++ 15_final/src/entities/Assets.java
@@ -12,7 +12,7 @@ import board.Street;
  */
 
 public class Assets {
-	
+
 	ArrayList<Ownable> owned = new ArrayList<Ownable>();
 	ArrayList<Street> ownedStreet = new ArrayList<Street>();
 	ArrayList<String> property = new ArrayList<String>();
@@ -20,218 +20,280 @@ public class Assets {
 	private boolean propertyOwner;
 	private boolean buildingOwner;
 	private boolean buildStatus;
-	
+
 	public Assets() {
 		jailCard = 0;
 		propertyOwner = false;
 		buildingOwner = false;
 		buildStatus = false;
 	}
-	
+
 	/**
 	 * Method for adding a get out of jail free card to the player
-	 * @param jailCard 
+	 * 
+	 * @param jailCard
 	 */
-	
+
 	public void setJailCard() {
 		jailCard++;
 	}
-	
+
 	/**
 	 * Method for checking if the player has a get out of jail free card
-	 * @return Boolean value true or false depending on whether or not the player has a card
+	 * 
+	 * @return Boolean value true or false depending on whether or not the
+	 *         player has a card
 	 */
-	
+
 	public boolean getJailCard() {
-		if (jailCard>=1) {
+		if (jailCard >= 1) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
-	
+
 	/**
-	 * Method for removing a get out of jail free card after it is used by the player
-	 * @param jailCard 
+	 * Method for removing a get out of jail free card after it is used by the
+	 * player
+	 * 
+	 * @param jailCard
 	 */
-	
+
 	public void useJailCard() {
 		jailCard--;
 	}
-	
+
 	/**
 	 * Method for adding a bought square to list of squares a player owns
+	 * 
 	 * @param square
 	 */
-	
+
 	public void buySquare(Ownable square) {
 		owned.add(square);
 		property.add(square.toString());
 		propertyOwner = true;
-		
+
 		if (square instanceof Street) {
-			
-			Street isStreet = (Street)square;
+
+			Street isStreet = (Street) square;
 			ownedStreet.add(isStreet);
-			
-		}
-			
+
 		}
 
+	}
 
 	/**
 	 * Method for determining the square IDs of the squares a player owns
+	 * 
 	 * @return An integer array with the square IDs
 	 */
-	
+
 	public int[] getOwnedID() {
-		
+
 		int[] squareID = new int[owned.size()];
-		
-		for (int i=0; i <= owned.size(); i++) {
-			
+
+		for (int i = 0; i <= owned.size(); i++) {
+
 			squareID[i] = owned.get(i).getID();
 		}
-		
+
 		return squareID;
-		
+
 	}
-	
-	public ArrayList <Ownable> getOwned(){
+
+	public ArrayList<Ownable> getOwned() {
 		return owned;
 	}
-	
+
 	public ArrayList<String> getPropertyList() {
 		return property;
 	}
-	
+
 	public String[] getHouseList() {
-		
+
 		String[] houseList = new String[owned.size()];
-		
-		for (int i=0; i <= ownedStreet.size(); i++) {
-			
+
+		for (int i = 0; i <= ownedStreet.size(); i++) {
+
 			if (ownedStreet.get(i).getNumberOfBuildings() >= 1 && ownedStreet.get(i).getNumberOfBuildings() < 5) {
-			
-			houseList[i] = ownedStreet.get(i).toString();
-				
+
+				houseList[i] = ownedStreet.get(i).toString();
+
 			}
-			
+
 		}
-		
+
 		return houseList;
 	}
-	
+
 	public String[] getHotelList() {
-		
+
 		String[] hotelList = new String[owned.size()];
-		
-		for (int i=0; i <= ownedStreet.size(); i++) {
-			
+
+		for (int i = 0; i <= ownedStreet.size(); i++) {
+
 			if (ownedStreet.get(i).getNumberOfBuildings() == 5) {
-			
-			hotelList[i] = ownedStreet.get(i).toString();
-				
+
+				hotelList[i] = ownedStreet.get(i).toString();
+
 			}
-			
+
 		}
-		
+
 		return hotelList;
 	}
-	
+
 	public boolean getBuilding() {
-		
+
 		return buildingOwner;
-		
+
 	}
-	
+
 	public boolean getProperty() {
-		
+
 		return propertyOwner;
-		
+
 	}
-	
+
 	public void buyHouses(Street street, int amount) {
-		
+
 		street.buyHouses(amount);
 		buildingOwner = true;
-		
+
 	}
-	
+
 	public void removeHouses(Street street, int amount) {
-		
+
 		boolean noBuilding = true;
 		buildingOwner = false;
-		
+
 		street.removeHouses(amount);
-		
-		for (int i=0; i <= ownedStreet.size(); i++) {
-			
+
+		for (int i = 0; i <= ownedStreet.size(); i++) {
+
 			if (ownedStreet.get(i).getNumberOfBuildings() >= 1) {
-			
-			noBuilding = false;
-				
+
+				noBuilding = false;
+
 			}
-			
+
 			if (noBuilding == false) {
-				
+
 				buildingOwner = true;
-				
+
 			}
-			
+
 		}
 	}
-	
+
 	public boolean getBuildStatus() {
-			
-		int counterA=0;
-		int counterB=0;
-		int counterC=0;
-		int counterD=0;
-		int counterE=0;
-		int counterF=0;
-		int counterG=0;
-		int counterH=0;
-		
-		for(int i =0; i<=ownedStreet.size();i++){
-			
-			if(ownedStreet.get(i).getType()=='A'){
+
+		int counterA = 0;
+		int counterB = 0;
+		int counterC = 0;
+		int counterD = 0;
+		int counterE = 0;
+		int counterF = 0;
+		int counterG = 0;
+		int counterH = 0;
+
+		for (int i = 0; i <= ownedStreet.size(); i++) {
+
+			if (ownedStreet.get(i).getType() == 'A') {
 				counterA++;
-			
-			}else if (ownedStreet.get(i).getType()=='B'){
+
+			} else if (ownedStreet.get(i).getType() == 'B') {
 				counterB++;
-				
-			}else if (ownedStreet.get(i).getType()=='C'){
+
+			} else if (ownedStreet.get(i).getType() == 'C') {
 				counterC++;
-				
-			}else if (ownedStreet.get(i).getType()=='D'){
+
+			} else if (ownedStreet.get(i).getType() == 'D') {
 				counterD++;
-				
-			}else if (ownedStreet.get(i).getType()=='E'){
+
+			} else if (ownedStreet.get(i).getType() == 'E') {
 				counterE++;
-				
-			}else if (ownedStreet.get(i).getType()=='F'){
+
+			} else if (ownedStreet.get(i).getType() == 'F') {
 				counterF++;
-				
-			}else if (ownedStreet.get(i).getType()=='G'){
+
+			} else if (ownedStreet.get(i).getType() == 'G') {
 				counterG++;
-				
-			}else if (ownedStreet.get(i).getType()=='H'){
+
+			} else if (ownedStreet.get(i).getType() == 'H') {
 				counterH++;
+			}
+
 		}
-			
-				}
-				
-		if (counterA==2 || counterH==2) {
+
+		if (counterA == 2 || counterH == 2) {
 			buildStatus = true;
-		} else if (counterB==3 || counterC==3 || counterD==3 || counterE==3 || counterF==3 || counterG==3) {
+		} else if (counterB == 3 || counterC == 3 || counterD == 3 || counterE == 3 || counterF == 3 || counterG == 3) {
 			buildStatus = true;
 		} else {
 			buildStatus = false;
 		}
-			
+
 		return buildStatus;
-		
+
 	}
-	
+
+	public String[] getBuildableList() {
+
+		String[] buildableList = new String[ownedStreet.size()];
+		int counterA = 0;
+		int counterB = 0;
+		int counterC = 0;
+		int counterD = 0;
+		int counterE = 0;
+		int counterF = 0;
+		int counterG = 0;
+		int counterH = 0;
+
+		for (int i = 0; i <= ownedStreet.size(); i++) {
+
+			if (ownedStreet.get(i).getType() == 'A') {
+				counterA++;
+
+			} else if (ownedStreet.get(i).getType() == 'B') {
+				counterB++;
+
+			} else if (ownedStreet.get(i).getType() == 'C') {
+				counterC++;
+
+			} else if (ownedStreet.get(i).getType() == 'D') {
+				counterD++;
+
+			} else if (ownedStreet.get(i).getType() == 'E') {
+				counterE++;
+
+			} else if (ownedStreet.get(i).getType() == 'F') {
+				counterF++;
+
+			} else if (ownedStreet.get(i).getType() == 'G') {
+				counterG++;
+
+			} else if (ownedStreet.get(i).getType() == 'H') {
+				counterH++;
+			}
+
+		}
+		for (int i = 0; i <= ownedStreet.size(); i++) {
+
+			if (counterA == 2 && ownedStreet.get(i).getType() == 'A'
+					|| counterB == 3 && ownedStreet.get(i).getType() == 'B'
+					|| counterC == 3 && ownedStreet.get(i).getType() == 'C'
+					|| counterD == 3 && ownedStreet.get(i).getType() == 'D'
+					|| counterE == 3 && ownedStreet.get(i).getType() == 'E'
+					|| counterF == 3 && ownedStreet.get(i).getType() == 'F'
+					|| counterG == 3 && ownedStreet.get(i).getType() == 'G'
+					|| counterH == 2 && ownedStreet.get(i).getType() == 'H') {
+				buildableList[i] = ownedStreet.get(i).toString();
+			}
+
+		}
+		return buildableList;
+	}
 }
---
+++ 15_final/src/test/Buildable.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import entities.Player;
+
+public class Buildable {
+
+	@Test
+	public void test() {
+		
+		Player p1 = new Player("Simon");
+		
+		p1.buySquare(square);
+		
+		
+	}
+
+}
