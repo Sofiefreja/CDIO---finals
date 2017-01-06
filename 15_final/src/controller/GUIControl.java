f8b3d2738c98333b0478a65a560eceff0a671650
emilyrasmussen <emilyrasmussen@emilys-air.clients.wireless.dtu.dk>
2017-01-06 13:00:49 +0100

    Fix

--- 15_final/src/controller/GUIControl.java
+++ 15_final/src/controller/GUIControl.java
@@ -5,14 +5,11 @@ import java.awt.*;
 import java.util.ArrayList;
 //import board.*;
 import desktop_fields.*;
-<<<<<<< HEAD
 import desktop_fields.Jail;
 import desktop_fields.Start;
-=======
 import desktop_fields.Brewery;
 import desktop_fields.Shipping;
 import desktop_fields.Street;
->>>>>>> branch 'Develop' of https://github.com/Sofiefreja/CDIO---finals.git
 import desktop_fields.Tax;
 //import desktop_fields.Refuge;
 //import desktop_fields.Tax;
@@ -107,7 +104,7 @@ public class GUIControl {
 				.setBgColor(Color.getHSBColor(241, 196, 15)).setFgColor(Color.black).build();
 		fields[37] = new Street.Builder().setTitle("Frederiksberggade").setDescription("Leje af grund 700,  m/1 hus = 3500, 2 huse = 10000, 3 huse = 22000, 4 huse = 30000, hotel = 40000, Hus pris = 4000, Hotel pris = 4000, pant = 3500").setSubText("Pris: 7000 kr.")
 				.setBgColor(Color.orange).setFgColor(Color.black).build();
-		fields[38] = new Tax.Builder().setTitle("Ekstraordinær statsskat").setDescription("Betal ekstraordinær statsskat 2000 kr.").setSubText("Betal 2000 kr.")
+		fields[38] = new Tax.Builder().setTitle("").setDescription("Betal ekstraordinær statsskat 2000 kr.").setSubText("Betal ekstraordinær statsskat 2000 kr.")
 				.setBgColor(Color.getHSBColor(241, 196, 15)).setFgColor(Color.black).build();
 		fields[39] = new Street.Builder().setTitle("Rådhuspladsen").setDescription("Leje af grund 1000,  m/1 hus = 4000, 2 huse = 12000, 3 huse = 28000, 4 huse = 34000, hotel = 40000, Hus pris = 4000, Hotel pris = 4000, pant = 4000").setSubText("Pris: 8000 kr.")
 				.setBgColor(Color.orange).setFgColor(Color.black).build();
