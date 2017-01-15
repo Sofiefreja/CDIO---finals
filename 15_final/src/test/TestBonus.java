074d8d5a60f898de7369e3136827f42437df0eaf
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2017-01-15 14:40:46 +0100

    corrected testbonus and deleted empty fakecuptest

--- 15_final/src/test/TestBonus.java
+++ 15_final/src/test/TestBonus.java
@@ -2,42 +2,100 @@ package test;
 
 
 import entities.Player;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 
 public class TestBonus {
 
-	Player p1 = new Player("Simon", 100000);
-
+	Player p1;
+	String name="Simon";
+	int initBalance=0;
+	int orgCurrentPosition=33;
+	int orgPreviousPosition=27;
+	
+	@Before
+	public void setUp(){
+		p1= new Player(name, initBalance);
+		p1.setPosition(orgCurrentPosition, orgPreviousPosition);
+		assertTrue(p1.toString().equals(name));
+		assertFalse(p1.getJailStatus());
+		assertEquals(p1.getBalance(),initBalance);
+		assertEquals(p1.getCurrentPosition(),orgCurrentPosition);
+		assertEquals(p1.getPreviousPosition(),orgPreviousPosition);
+	}
+	@After
+	public void tearDown(){
+		p1 = new Player(name,initBalance);
+		p1.setPosition(orgCurrentPosition, orgPreviousPosition);
+	}
 	@Test
-	public void test() {
-
-		boolean jailStatus = false;
-
-		int penultimatePosition = 17;
-
-		int currentPosition = 29;
-
-		int value = 12;
-
-		int previousPosition = currentPosition;
-
-		System.out.print((currentPosition + value) % 40);
-
-		currentPosition = (currentPosition + value) % 40;
-
-		if (penultimatePosition >= 16 && previousPosition >= 28 && currentPosition <= 12 && jailStatus == false) {
-
-			System.out.println("Derp");
-
+	public void conditions1() {
+		int rollValue = 4;
+		p1.setFirstRound(false);
+		p1.setPosition(p1.getCurrentPosition()+rollValue, p1.getCurrentPosition());
+		//uses the same algorithm as in GUIControl.moveVehicle()
+		for (int i = 0; i < rollValue; i++) {//Sets the player on one spot after another to simulate a piece moving on the board
+			if ((p1.getPreviousPosition() + i + 1) % 40 + 1 == 2) {//if the player has passed the Start field
+				if (p1.getFirstRound() == false && p1.getCurrentPosition() != 10) {
+					p1.deposit(4000);
+				} else {
+					p1.setFirstRound(false);
+				}
+			}
 		}
-
-		value = 8;
-
-		if (penultimatePosition >= 16 && previousPosition >= 28 && currentPosition <= 12 && jailStatus == false) {
-
-			System.out.println("Derp2");
-
+		assertFalse(p1.getBalance()>initBalance);
+	}
+	@Test
+	public void conditions2(){
+		int rollValue=7;
+		p1.setFirstRound(false);
+		p1.setPosition(p1.getCurrentPosition()+rollValue,p1.getCurrentPosition());
+		for (int i = 0; i < rollValue; i++) {//Sets the player on one spot after another to simulate a piece moving on the board
+			if ((p1.getPreviousPosition() + i + 1) % 40 + 1 == 2) {//if the player has passed the Start field
+				if (p1.getFirstRound() == false && p1.getCurrentPosition() != 10) {
+					p1.deposit(4000);
+				} else {
+					p1.setFirstRound(false);
+				}
+			}
 		}
-
+		assertFalse(p1.getBalance()>initBalance);
+	}
+	@Test
+	public void conditions3(){
+		int rollValue=10;
+		p1.setFirstRound(false);
+		p1.setPosition(p1.getCurrentPosition()+rollValue,p1.getCurrentPosition());
+		for (int i = 0; i < rollValue; i++) {//Sets the player on one spot after another to simulate a piece moving on the board
+			if ((p1.getPreviousPosition() + i + 1) % 40 + 1 == 2) {//if the player has passed the Start field
+				if (p1.getFirstRound() == false && p1.getCurrentPosition() != 10) {
+					p1.deposit(4000);
+				} else {
+					p1.setFirstRound(false);
+				}
+			}
+		}
+		assertTrue(p1.getBalance()>initBalance);
+	}
+	@Test
+	public void conditions4(){
+		p1.setPosition(0, 0);
+		int rollValue=10;
+		p1.setPosition(10, 0);
+		p1.setPosition(p1.getCurrentPosition()+rollValue,p1.getCurrentPosition());
+		for (int i = 0; i < rollValue; i++) {//Sets the player on one spot after another to simulate a piece moving on the board
+			if ((p1.getPreviousPosition() + i + 1) % 40 + 1 == 2) {//if the player has passed the Start field
+				if (p1.getFirstRound() == false && p1.getCurrentPosition() != 10) {
+					p1.deposit(4000);
+				} else {
+					p1.setFirstRound(false);
+				}
+			}
+		}
+		assertFalse(p1.getBalance()>initBalance);
 	}
 }
